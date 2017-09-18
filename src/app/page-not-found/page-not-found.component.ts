7386802ce451f5243c65646022d58674e7401036
Nui Rattapon <iwalker.foto@gmail.com>
2017-09-19 00:53:24 +1200

    minimize page not found

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -2,8 +2,24 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
-  templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.css']
+  template: `
+    <div class="container pagenotfound">
+      <section class="error404">
+        <h1>Page not found</h1>
+      </section>
+    </div>
+  `,
+  styles: [`
+  .pagenotfound {
+    height: 40vh;
+    display: table;
+    text-align: center;
+  }
+  .error404 {
+    display: table-cell;
+    vertical-align: middle;
+  }
+  `]
 })
 export class PageNotFoundComponent implements OnInit {
 
